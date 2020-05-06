ec75ffc7b30d01bdbdb56f0984f0e4c937b0eff7
leejisu <wltn2814@naver.com>
2020-05-06 23:45:52 +0900

    blue 2차 수정(코드 오류 수정 + 예외 처리 추가)

--- 멋사 Python 과제.py
+++ 멋사 Python 과제.py
@@ -1,37 +1,44 @@
-#a[num]으로 class를 list로 나타내는게 문제이지 않을까 싶어요... 그거 고민중이었습니다!
 
 class Bankaccount: #계좌 class 선언
     name = "익명인"
     account = 1
     money = 0
-    def __init__(self, name, account):
+    def __init__(self, name, account, money):
         self.account = account
         self.name = name
         self.money = money
-        print("계좌개설을 완료하였습니다##")
+        print("##계좌개설을 완료하였습니다##")
     def in_money(self, name, account):
-        if((strself.name == name)&(self.account == account)): #계좌랑 이름 일치하는지 확인
-             add = input("입금하실 금액을 입력해주세요 : ")
-             self.money += add
+        if((self.name == name)&(self.account == account)): #계좌랑 이름 일치하는지 확인
+             print("계좌잔고: %d 원"%(self.money))
+             add = int(input("입금하실 금액을 입력해주세요 : "))
+             if(add <0):
+                 print("**잘못 입력하셨습니다**")
+                 return 0
+             self.money += int(add)
              print("##계좌잔고: ",self.money,"원##")
              print("##입금이 완료되었습니다##")
         else:
             print("**잘못 입력하셨습니다**")
     def out_money(self, name, account):
         if((self.name == name)&(self.account == account)):  #계좌랑 이름 일치하는지 확인
-             sub = input("출금하실 금액을 입력해주세요 : ")
+             print("계좌잔고: %d 원"%(self.money))
+             sub = int(input("출금하실 금액을 입력해주세요 : "))
              if (sub > self.money):
                  print("##잔고가 부족하여 출금에 실패하였습니다##")
-             self.money -= sub
+                 return 0
+             if(sub<0):
+                 print("**잘못 입력하셨습니다**")
+                 return 0
+             self.money -= int(sub)
              print("##계좌잔고: ",self.money,"원##")
              print("##출금이 완료되었습니다##")
         else:
             print("**잘못 입력하셨습니다**")
-    def list_account(self, p): #계좌 출력
-        print("계좌번호: %d / 이름: %s / 잔액: %d 원"%(p.account, p.name, p.money))
-    def end_system(self):
-        return 5
-        
+    def list_account(self): #계좌 출력
+        print("계좌번호: %s / 이름: %s / 잔액: %d 원"%(self.account, self.name, self.money))
+
+a = []
 number = 0    #프로그램 종료 변수     
 num = 0; #계좌 총 개수
 while number!=5 :
@@ -47,10 +54,13 @@ while number!=5 :
         print("\n======계좌개설======")
         account = input("계좌번호: ")
         name = input("이름: ")
-        money = input("예금: ")
-        a[num]=Bankaccount(name, account) 
-        num = num + 1 #계좌 총 개수 증가!
-        print("====================")
+        money = int(input("예금: "))
+        if (money>=0):
+            a.insert(num,Bankaccount(name, account, money))
+            num = num + 1 #계좌 총 개수 증가!
+        else:
+            print("**잘못 입력하셨습니다**")
+        print("====================\n")
     elif n=='2':
         ac = -1
         print("\n======입금하기======")
@@ -60,10 +70,10 @@ while number!=5 :
             if(a[i].account==account):
                 ac = i
         if(ac == -1):#일치하는 계좌가 없는 경우
-            print("**잘못 입력하셨습니다**")
+            print("**잘못 입력하셨습니다**1")
         else: #일치하는 계좌가 있는 경우
-            in_money(name, account)
-        print("====================")
+            a[i].in_money(name, account)
+        print("====================\n")
     elif n=='3' :
         ac = -1
         print("\n======출금하기======")
@@ -75,23 +85,14 @@ while number!=5 :
         if(ac == -1):  #일치하는 계좌가 없는 경우
             print("**잘못 입력하셨습니다**")
         else: #일치하는 계좌가 있는 경우
-            out_money(name, account)
-        print("====================")
+            a[i].out_money(name, account)
+        print("====================\n")
     elif n=='4' :
         print("\n======전체조회======")
-        for i in range(0,num+1):#모든 계좌들 출력
-            list_account(a[i])
+        for i in range(0,num):#모든 계좌들 출력
+            a[i].list_account()
+        print("====================\n")
     elif n=='5' : 
-        number = end_system()
+        number = 5
     else:
-        print("\n**잘못 입력하셨습니다**")
-
-        
-            
-            
-        
-        
-        
-       
-        
-    
+        print("\n**잘못 입력하셨습니다**\n")
