05ff08f15404b830046fa3cf027efc59e28605a4
polcomlicepute <min199912@naver.com>
2020-05-07 18:52:24 +0900

    수정

--- eh.py
+++ eh.py
@@ -46,3 +46,67 @@ class Bankaccount: #계좌 class 선언
             print("**잘못 입력하셨습니다**")
     def list_account(self): #계좌 출력
         print("계좌번호: %s / 이름: %s / 잔액: %d 원"%(self.account, self.name, self.money))
+
+class Start:
+    a=[]
+    num=0
+    def __init__(self, a,num):
+        self.a=a
+        self.num=num
+   
+      #계좌 총 개수
+    def one(self,a, num):
+        print("\n======계좌개설======")
+        account = input("계좌번호: ")
+        if account.isdecimal():
+            name = input("이름: ")
+            isnum=input("예금: ")
+            if isnum.isdecimal():
+                money = int(isnum)
+                if (money>=0):
+                    self.a.insert(self.num,Bankaccount(name, account, money))
+                    self.num= self.num+ 1
+                    #s=num+1
+                    # #계좌 총 개수 증가!
+                else:
+                    print("**잘못 입력하셨습니다**")
+            else:
+                print("**잘못 입력하셨습니다**")
+        else:
+            print("**잘못 입력하셨습니다**")
+        
+        print("====================\n")
+    def two(self,a, num):
+        ac = -1
+        print("\n======입금하기======")
+        account = input("입금하실 계좌 번호를 입력해주세요: ")
+        name = input("계좌이름: ")
+        for i in range(0,self.num):#계좌 번호와 일치하는 class변수 찾기
+            if(self.a[i].account==account):
+                ac = i
+        if(ac == -1):#일치하는 계좌가 없는 경우
+            print("**잘못 입력하셨습니다**1")
+        else: #일치하는 계좌가 있는 경우
+            self.a[i].in_money(name, account)
+        print("====================\n")
+    def three(self,a, num):
+        ac = -1
+        print("\n======출금하기======")
+        account = input("출금하실 계좌 번호를 입력해주세요: ")
+        name = input("계좌이름: ")
+        for i in range(0,self.num): #계좌 번호와 일치하는 class변수 찾기
+            if(self.a[i].account==account):
+                ac = i
+        if(ac == -1):  #일치하는 계좌가 없는 경우
+            print("**잘못 입력하셨습니다**")
+        else: #일치하는 계좌가 있는 경우
+            self.a[i].out_money(name, account)
+        print("====================\n")
+    def four(self,a, num):
+        print("\n======전체조회======")
+        for i in range(0,self.num):#모든 계좌들 출력
+            self.a[i].list_account()
+        print("====================\n")
+
+        
+
--- 멋사 Python 과제.py
+++ 멋사 Python 과제.py
@@ -1,9 +1,11 @@
 from eh import Bankaccount
+from eh import Start
 
+a=[]
+num=0
+st = Start(a,num)
 
-a = []
-number = 0    #프로그램 종료 변수     
-num = 0; #계좌 총 개수
+number = 0    #프로그램 종료 변수 
 while number!=5 :
     print("======Bank Menu=====")
     print("1. 계좌개설")
@@ -14,48 +16,15 @@ while number!=5 :
     print("====================")
     n = input("입력: ")
     if n=='1':
-        print("\n======계좌개설======")
-        account = input("계좌번호: ")
-        name = input("이름: ")
-        money = int(input("예금: "))
-        if (money>=0):
-            a.insert(num,Bankaccount(name, account, money))
-            num = num + 1 #계좌 총 개수 증가!
-        else:
-            print("**잘못 입력하셨습니다**")
-        print("====================\n")
+       st.one(a,num)
     elif n=='2':
-        ac = -1
-        print("\n======입금하기======")
-        account = input("입금하실 계좌 번호를 입력해주세요: ")
-        name = input("계좌이름: ")
-        for i in range(0,num):#계좌 번호와 일치하는 class변수 찾기
-            if(a[i].account==account):
-                ac = i
-        if(ac == -1):#일치하는 계좌가 없는 경우
-            print("**잘못 입력하셨습니다**1")
-        else: #일치하는 계좌가 있는 경우
-            a[i].in_money(name, account)
-        print("====================\n")
+        st.two(a, num)
     elif n=='3' :
-        ac = -1
-        print("\n======출금하기======")
-        account = input("출금하실 계좌 번호를 입력해주세요: ")
-        name = input("계좌이름: ")
-        for i in range(0,num): #계좌 번호와 일치하는 class변수 찾기
-            if(a[i].account==account):
-                ac = i
-        if(ac == -1):  #일치하는 계좌가 없는 경우
-            print("**잘못 입력하셨습니다**")
-        else: #일치하는 계좌가 있는 경우
-            a[i].out_money(name, account)
-        print("====================\n")
+        st.three(a, num)
     elif n=='4' :
-        print("\n======전체조회======")
-        for i in range(0,num):#모든 계좌들 출력
-            a[i].list_account()
-        print("====================\n")
+        st.four(a, num)
     elif n=='5' : 
         number = 5
     else:
         print("\n**잘못 입력하셨습니다**\n")
+        
