135d9c0ca8099c67236778b933233f48406f569a
polcomlicepute <min199912@naver.com>
2020-05-14 20:52:37 +0900

    account 삭제기능

---
+++ ham_2주차/eh.py
@@ -0,0 +1,141 @@
+class Bankaccount: #계좌 class 선언
+    name = "익명인"
+    account = 1
+    money = 0
+    def __init__(self, name, account, money):
+        self.account = account
+        self.name = name
+        self.money = money
+        print("##계좌개설을 완료하였습니다##")
+    def in_money(self, name, account):
+        if((self.name == name)&(self.account == account)): #계좌랑 이름 일치하는지 확인
+             print("계좌잔고: %d 원"%(self.money))
+             isnum=input("입금하실 금액을 입력해주세요 : ")
+             if isnum.isdecimal():
+                add = int(isnum)
+                if(add <0):
+                     print("**잘못 입력하셨습니다**")
+                     return 0
+                self.money += int(add)
+                print("##계좌잔고: ",self.money,"원##")
+                print("##입금이 완료되었습니다##")
+             else:
+                print("**잘못 입력하셨습니다. 숫자로 입력해주세요^^**")
+                return 0
+        else:
+            print("**잘못 입력하셨습니다**")
+    def out_money(self, name, account):
+        if((self.name == name)&(self.account == account)):  #계좌랑 이름 일치하는지 확인
+             print("계좌잔고: %d 원"%(self.money))
+             isnum=input("출금하실 금액을 입력해주세요 : ")
+             if isnum.isdecimal():
+                sub = int(isnum)
+                if (sub > self.money):
+                    print("##잔고가 부족하여 출금에 실패하였습니다##")
+                    return 0
+                if(sub<0):
+                    print("**잘못 입력하셨습니다**")
+                    return 0
+                self.money -= int(sub)
+                print("##계좌잔고: ",self.money,"원##")
+                print("##출금이 완료되었습니다##")
+             else:
+                print("**잘못 입력하셨습니다. 숫자로 입력해주세요^^**")
+                return 0
+        else:
+            print("**잘못 입력하셨습니다**")
+    def list_account(self): #계좌 출력
+        if self.account != None:
+            print("계좌번호: %s / 이름: %s / 잔액: %d 원"%(self.account, self.name, self.money))
+        else:
+            pass
+    def del_money(self, name, account):
+        if((self.name == name)&(self.account == account)):  #계좌랑 이름 일치하는지 확인
+                self.account = None
+                self.name = None
+                self.money = None
+                print("##삭제가 완료되었습니다##")
+        else:
+            print("**잘못 입력하셨습니다**")
+            
+                
+
+class Start:
+    a = []
+    num = 0
+    def __init__(self):
+        self.a=[]
+        self.num=0
+   
+      #계좌 총 개수
+    def one(self):
+        print("\n======계좌개설======")
+        account = input("계좌번호: ")
+        if account.isdecimal():
+            name = input("이름: ")
+            isnum=input("예금: ")
+            if isnum.isdecimal():
+                money = int(isnum)
+                if (money>=0):
+                    self.a.insert(self.num,Bankaccount(name, account, money))
+                    self.num= self.num+ 1
+                    #s=num+1
+                    # #계좌 총 개수 증가!
+                else:
+                    print("**잘못 입력하셨습니다**")
+            else:
+                print("**잘못 입력하셨습니다**")
+        else:
+            print("**잘못 입력하셨습니다**")
+        
+        print("====================\n")
+    def two(self):
+        ac = -1
+        print("\n======입금하기======")
+        account = input("입금하실 계좌 번호를 입력해주세요: ")
+        name = input("계좌이름: ")
+        for i in range(0, self.num):  # 계좌 번호와 일치하는 class변수 찾기
+            if (self.a[i].account == account and self.a[i].name==name):
+                ac = i
+                break
+        if(ac == -1):#일치하는 계좌가 없는 경우
+            print("**잘못 입력하셨습니다**")
+        else: #일치하는 계좌가 있는 경우
+            self.a[i].in_money(name, account)
+        print("====================\n")
+    def three(self):
+        ac = -1
+        print("\n======출금하기======")
+        account = input("출금하실 계좌 번호를 입력해주세요: ")
+        name = input("계좌이름: ")
+        for i in range(0, self.num):  # 계좌 번호와 일치하는 class변수 찾기
+            if (self.a[i].account == account and self.a[i].name==name):
+                ac = i
+                break
+        if(ac == -1):  #일치하는 계좌가 없는 경우
+            print("**잘못 입력하셨습니다**")
+        else: #일치하는 계좌가 있는 경우
+            self.a[ac].out_money(name, account)
+        print("====================\n")
+    def four(self):
+        print("\n======전체조회======")
+        for i in range(0,self.num):#모든 계좌들 출력
+            self.a[i].list_account()
+        print("====================\n")
+    def five(self):
+        ac = -1
+        print("\n======계좌 삭제하기======")
+        account = input("삭제하실 계좌 번호를 입력해주세요: ")
+        name = input("계좌이름: ")
+        for i in range(0, self.num):  # 계좌 번호와 일치하는 class변수 찾기
+            if (self.a[i].account == account and self.a[i].name==name):
+                ac = i
+                break
+        if(ac == -1):#일치하는 계좌가 없는 경우
+            print("**잘못 입력하셨습니다**")
+        else: #일치하는 계좌가 있는 경우
+            self.a[i].del_money(name, account)
+        print("====================\n")
+
+        
+
--- ham_2주차/멋사 Python 과제.py
+++ ham_2주차/멋사 Python 과제.py
@@ -4,13 +4,14 @@ from eh import Start
 st = Start()
 
 number = 0    #프로그램 종료 변수 
-while number!=5 :
+while number!=6 :
     print("======Bank Menu=====")
     print("1. 계좌개설")
     print("2. 입금하기")
     print("3. 출금하기")
     print("4. 전체조회")
-    print("5. 프로그램 종료")
+    print("5. 계좌 삭제")
+    print("6. 프로그램 종료")
     print("====================")
     n = input("입력: ")
     if n=='1':
@@ -22,7 +23,9 @@ while number!=5 :
     elif n=='4' :
         st.four()
     elif n=='5' : 
-        number = 5
+        st.five()
+    elif n=='6' :
+        number = 6
     else:
         print("\n**잘못 입력하셨습니다**\n")
         
